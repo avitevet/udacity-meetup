6008d1c7093335fdce4b00570743e5ab0bdf2ce2
Avi Tevet <github@avitevet.com>
2016-07-29 00:02:24 -0700

    Initial commit

---
+++ test/spec/test.js
@@ -0,0 +1,7 @@
+describe("Give it some context", function () {
+	describe('maybe a bit more context here', function () {
+		it('should run here few assertions', function () {
+			expect(1).toEqual(1);
+		});
+	});
+});
