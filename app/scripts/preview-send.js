38ab7554f3be4e135d8f92f49835fcb679973e7e
Avi Tevet <github@avitevet.com>
2016-08-19 14:35:37 -0700

    Nicely formatted dates on preview page, and nicely formatted message

--- app/scripts/preview-send.js
+++ app/scripts/preview-send.js
@@ -1,10 +1,25 @@
 (function() {
 	var inviteList = new InviteList('#inviteList', InviteListMode.RO);
 
-	['eventType', 'eventName', 'host', 'location', 'message'].forEach(function(id) {
+	['eventType', 'eventName', 'host', 'location'].forEach(function(id) {
 		document.querySelector('#' + id).innerHTML = sessionStorage[id];
 	});
 
-	// format the dates
+	document.querySelector('#message').innerHTML = sessionStorage['message'].replace(/\n/g, '<br>');
 
+	// format the dates
+	var startDateTime = moment(sessionStorage.startDateTime),
+		endDateTime = moment(sessionStorage.endDateTime);
+
+	// if they're on the same day, then use this format:
+	// DayName, Month day year, startTime - endTime
+	//
+	// if they're on different days, then use this format:
+	// DayName, Month day year, startTime - DayName, Month day year, endTime
+	var startPart = startDateTime.format('dddd MMM D, YYYY, h:mmA');
+	var endPart = endDateTime.format('h:mmA');
+	if (!startDateTime.isSame(endDateTime, 'day')) {
+		endPart = endDateTime.format('dddd MMM D, YYYY, h:mmA');
+	}
+	document.querySelector('#eventDates').innerHTML = startPart + ' - ' + endPart;
 })();
