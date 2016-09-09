7b46cbe1277035f39099616720cdfd66477aac67
Avi Tevet <github@avitevet.com>
2016-09-08 22:17:29 -0700

    Create a JS component for myevents

--- app/scripts/my-events.js
+++ app/scripts/my-events.js
@@ -1,5 +1,18 @@
+/* global moment */
+
+var MyEvents = function(root) {
+	if (typeof(root) === 'string') {
+		root = document.querySelector(root);
+	}
+
+	root.querySelector('#eventName').innerHTML = sessionStorage.eventName;
+	root.querySelector('#numNoResponse').innerHTML = JSON.parse(sessionStorage.invitedEntries).length;
+	root.querySelector('#eventStartDate').innerHTML = moment(sessionStorage.startDateTime).calendar();
+};
+
 (function() {
-	document.querySelector('#eventName').innerHTML = sessionStorage.eventName;
-	document.querySelector('#numNoResponse').innerHTML = JSON.parse(sessionStorage.invitedEntries).length;
-	document.querySelector('#eventStartDate').innerHTML = moment(sessionStorage.startDateTime).calendar();
+	var root = document.querySelector('#myevents');
+	if (root) {
+		new MyEvents(root);
+	}
 })();
