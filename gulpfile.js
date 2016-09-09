20ff37076cdce88d730ef960ec49f201edc99677
Avi Tevet <github@avitevet.com>
2016-09-08 22:32:48 -0700

    Use app.min.js in all html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 // generated on 2016-07-25 using generator-webapp 2.1.0
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
@@ -24,15 +26,52 @@ gulp.task('styles', () => {
     .pipe(reload({stream: true}));
 });
 
-gulp.task('scripts', () => {
-	return gulp.src('app/scripts/**/*.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
+/**
+ * Concat, babel, and uglify the given source files in the project.
+ * Store them in the given dest directory in a file named filename.
+ * smap indicates whether the files should have source maps or not -
+ * in production mode, the files should not have sourcemaps, otherwise they should.
+ *
+ * @param src  gulp.src argument
+ * @param smap  Set to true to include sourcemaps, false otherwise
+ * @param filename  Name of the concatenated/babeled/uglified file
+ * @param dest  Directory where concatenated/babeled/uglified file is stored
+ *
+ * @return object suitable for returning in a gulp task
+ */
+function scripts(src, smap, filename, dest) {
+	var s = gulp.src(src)
+    .pipe($.plumber());
+
+	if (smap) {
+		s = s.pipe($.sourcemaps.init());
+	}
+	s = s.pipe($.concat(filename))
     .pipe($.babel())
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/scripts'))
+		.pipe($.uglify());
+	if (smap) {
+		s = s.pipe($.sourcemaps.write('.'));
+	}
+
+	return s.pipe(gulp.dest(dest))
     .pipe(reload({stream: true}));
+}
+
+var scriptnames = ['main.js', 'invitelist.js', 'authenticationForm.js', 'create-event.js',
+	'invite-guests.js', 'my-events.js', 'preview-send.js'];
+var scriptfiles = [];
+scriptnames.forEach(function(name) {
+	scriptfiles.push('app/scripts/' + name);
 });
+
+gulp.task('scripts', () => {
+	return scripts(scriptfiles, true, 'app.min.js', '.tmp/scripts');
+});
+
+gulp.task('scripts:dist', () => {
+	return scripts(scriptfiles, false, 'app.min.js', '.tmp/scripts');
+});
+
 
 function lint(files, options) {
 	return gulp.src(files)
@@ -65,7 +104,7 @@ gulp.task('test', function() {
 		keepRunner: 'test/generated',
 		includeStackTrace: true,
 		jasmineVersion: '2.4',
-		vendor: ['app/scripts/processEmails.js', 'app/scripts/invitelist.js']
+		vendor: ['app/scripts/invitelist.js', 'app/scripts/invite-guests.js']
 	}));
 });
 
